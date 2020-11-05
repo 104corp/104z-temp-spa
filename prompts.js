7f99062e31d8233e76a790df96077dfada13f9dd
Henry Chen <lb01910483@gmail.com>
2020-11-05 15:25:12 +0800

    [功能] 新增 devcontainer 選項

--- prompts.js
+++ prompts.js
@@ -19,6 +19,30 @@ module.exports = [
     default: false
   },
   {
+    name: "needDevContainer",
+    type: "confirm",
+    message: "是否需要創建 VS Code devContainer 環境 ( 包含 nginx 設定)",
+    default: false
+  },
+  {
+    name: "projectName",
+    type: "input",
+    message: "請輸入專案顯示名稱, ex: reviews (docker 名稱使用)",
+    default: "104z",
+    when: (input) => {
+      return input.needDevContainer
+    }
+  },
+  {
+    name: "nginxDomain",
+    type: "input",
+    message: "請輸入要攔截之 Domain, ex: reviews.104-dev.com.tw",
+    default: "/",
+    when: (input) => {
+      return input.needDevContainer
+    }
+  },
+  {
     name: 'plugins',
     type: 'checkbox',
     message: '安裝額外套件: ',
